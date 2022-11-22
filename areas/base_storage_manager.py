6a2e3beb2d3d53f9b6829e1197c53f10b0dc20a9
Vasiliy Shuvalov <vm.shuvalov@mail.ru>
2022-11-22 19:56:20 +0300

    Add base_storage_manager and upd class diagram

--- areas/base_storage_manager.py
+++ areas/base_storage_manager.py
@@ -1,2 +1,26 @@
+from base_storage_item import BaseStorageItem
+
+
 class BaseStorageManager:
-    ...
+    __items: list = None
+
+    def get_items(self) -> list:
+        return self.__items
+
+    def set_items(self, new_items: list):
+        if all(map(lambda new_item: isinstance(new_item, BaseStorageItem), new_items)):
+            self.__items = new_items
+        else:
+            raise TypeError
+
+    def add_item(self, new_item: BaseStorageItem):
+        if isinstance(new_item, BaseStorageItem):
+            self.__items.append(new_item)
+        else:
+            raise TypeError
+
+    def remove_item(self, item: BaseStorageItem):
+        if isinstance(item, BaseStorageItem):
+            self.__items.remove(item)
+        else:
+            raise TypeError
