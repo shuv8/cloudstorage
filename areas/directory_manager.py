ea8d34f66d16cddf03dec3e041f741854b12b81f
GOHARDGO <l2fonat@bk.ru>
2022-11-24 01:48:58 +0300

    Fixes, tests implementation

--- areas/directory_manager.py
+++ areas/directory_manager.py
@@ -1,54 +1,55 @@
-from base_storage_manager import BaseStorageManager
-from abstract_space import AbstractSpace
-from directory_manager import DirectoryManager
-from file_manager import FileManager
+from files import File
 from directory import Directory
+from files import FileManager
+
 
 class DirectoryManager:
     __items: list = None
-    __directoryManager: DirectoryManager = None
-    __fileManager: FileManager = None
+    __file_manager: FileManager = None
 
     def __init__(self):
         self.__items = []
 
     def create_dir(self, directory_name: str):
-        if self.directory_name #TO DO
-
-    def remove_dir(self, directory_name: str):
-            for item in self.__items:
-                if item.get_name()
-
-    def get_dir(self, directory_name: str) -> Directory:
-        for i in range(len(self.__items)):
-            if self.__items[i].get_directory_name() == directory_name:
-                return self.__items[i]
-        pass
-
-    def set_items(self, directory_name: str):
-        for i in range(len(self.__items)):
-            if self.__items[i].get_directory_name() == directory_name:
-                self.__items.pop(i)
-
-    def get_items(self, directory_name: str):
-        for i in range(len(self.__items)):
-            if self.__items[i].get_directory_name() == directory_name:
-                self.__items.pop(i)
-
-    def set_directory_manager(self, new_directory_manager: DirectoryManager):
-        if isinstance(new_directory_manager, DirectoryManager):
-            self.__directory_manager = new_directory_manager
+        if isinstance(directory_name, str):
+            dir_to_add = Directory()
+            dir_to_add.set_name(directory_name)
+            self.__items.append(dir_to_add)
         else:
             raise TypeError
 
-    def get_directory_manager(self) -> DirectoryManager:
-        return self.__directory_manager
+    def remove_dir(self, directory_name: str):
+        if isinstance(directory_name, str):
+            for i in range(len(self.__items)):
+                if self.__items[i].get_name() == directory_name:
+                    self.__items.pop(i)
+        else:
+            raise TypeError
+
+    def get_dir(self, directory_name: str) -> Directory:
+        if isinstance(directory_name, str):
+            for i in range(len(self.__items)):
+                if self.__items[i].get_name() == directory_name:
+                    return self.__items[i]
+            pass
+        else:
+            raise TypeError
+
+    # def set_items(self, items: list):
+    #     for item in items:
+    #         if isinstance(item, Directory) or isinstance(item, File):
+    #             self.__items.append(item)
+    #     else:
+    #         raise TypeError
+
+    def get_items(self) -> list:
+        return self.__items
 
     def set_file_manager(self, new_file_manager: FileManager):
         if isinstance(new_file_manager, FileManager):
-            self.__directory_manager = new_file_manager
+            self.__file_manager = new_file_manager
         else:
             raise TypeError
 
     def get_file_manager(self) -> FileManager:
-        return self.__fileManager
+        return self.__file_manager
