d673f19610e8a99fd4633e27664294eae408ec22
Roman Notkov <playkohb@yandex.ru>
2022-11-23 17:20:05 +0300

    change directory_manager

--- areas/test_directory_manager.py
+++ areas/test_directory_manager.py
@@ -1,15 +1,17 @@
 import pytest
 
 from directory_manager import DirectoryManager
-
-
 class TestDirectoryManager:
     @pytest.fixture(scope='function')
     def directory_manager(self):
         return DirectoryManager()
 
     def test_create_dir(self, directory_manager):
-        assert 1 == 1  # TODO
+        assert directory_manager.create_dir("asd") is None
+        try:
+            directory_manager.create_dir(123)
+        except TypeError:
+            pass
 
     def test_remove_dir(self, directory_manager):
         assert 1 == 1  # TODO
@@ -18,19 +20,31 @@ class TestDirectoryManager:
         assert 1 == 1  # TODO
 
     def test_set_items(self, directory_manager):
-        assert 1 == 1  # TODO
+        assert directory_manager.set_items([]) is None
+        try:
+            directory_manager.set_items("trq")
+        except TypeError:
+            pass
 
     def test_get_items(self, directory_manager):
         assert 1 == 1  # TODO
 
     def test_set_directory_manager(self, directory_manager):
-        assert 1 == 1  # TODO
+        assert directory_manager.set_items([]) is None
+        try:
+            directory_manager.set_items("trq")
+        except TypeError:
+            pass
 
     def test_get_directory_manager(self, directory_manager):
         assert 1 == 1  # TODO
 
     def test_set_file_manager(self, directory_manager):
-        assert 1 == 1  # TODO
+        assert directory_manager.set_items([]) is None
+        try:
+            directory_manager.set_items("trq")
+        except TypeError:
+            pass
 
     def test_get_file_manager(self, directory_manager):
         assert 1 == 1  # TODO
