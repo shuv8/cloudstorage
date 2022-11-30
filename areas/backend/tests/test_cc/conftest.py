5dbaa7c09a30738a0673dcfa47a70795427ec62f
Сергей Яценко <yatsenkoseryozha@gmail.com>
2022-11-30 14:24:19 +0300

    update test_cc client init

--- areas/backend/tests/test_cc/conftest.py
+++ areas/backend/tests/test_cc/conftest.py
@@ -1,8 +1,10 @@
 import pytest
 from web_server import app
 
-
 @pytest.fixture(scope='function')
 def app_client():
     app.testing = True
-    return app.test_client()
+    test_client = app.test_client()
+    login_data = {'email': 'test_mail@mail.com', 'password': 'password'}
+    test_client.put('/login', json=login_data)
+    return test_client
