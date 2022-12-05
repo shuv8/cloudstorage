63854b5e001e5b52823c7685934c0a73c1aac218
artem-nikolaev00 <starist2000@mail.ru>
2022-12-05 23:12:43 +0300

    update delete and download + update test

--- areas/backend/tests/test_cc/test_download.py
+++ areas/backend/tests/test_cc/test_download.py
@@ -1,11 +1,11 @@
 class TestDownloadController:
 
     def test_download_file(self, app_client_user):
-        response = app_client_user.get(path='/download/abd9cd7f-9ffd-41b0-bce4-eb14b51a6d71')
+        response = app_client_user.get(path='/download/abd9cd7f-9ffd-42b0-bce4-eb14b51a1fd1')
         assert response.status_code == 200
 
     def test_download_directory(self, app_client_user):
-        response = app_client_user.get(path='/download/4c3b76d1-fe24-4fdf-afdf-7c38adbdab15')
+        response = app_client_user.get(path='/download/bb01bafc-21f1-4af8-89f9-79aa0de840c0')
         assert response.status_code == 200
 
     def test_delete_not_found(self, app_client_user):
