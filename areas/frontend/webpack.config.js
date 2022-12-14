05169385dc37c1fa61d26dca704a3aa5ad8c92ea
Сергей Яценко <yatsenkoseryozha@gmail.com>
2022-12-14 11:18:16 +0300

    initial config

---
+++ areas/frontend/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-react', '@babel/preset-typescript'],
+                },
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.jsx', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'CloudStorage',
+            template: path.resolve(__dirname, 'src/index.html'),
+        }),
+    ],
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        port: 3000,
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+};
